884d8566985f68b644a14a963401aead72bc62aa
V.Seletskyi <v.seleckij@gmail.com>
2024-02-12 15:13:01 +0200

    MVPv2 -> changed generation logic

--- builder/agg.go
+++ builder/agg.go
@@ -19,40 +19,29 @@ func (*Agg) Matches(_ *MethodContext, source, target *xtype.Type) bool {
 // Build creates conversion source code for the given source and target type.
 func (*Agg) Build(gen Generator, ctx *MethodContext, sourceID *xtype.JenID, source, target *xtype.Type) ([]jen.Code, *xtype.JenID, *Error) {
 
-	// Get the current index from the context
-	index := ctx.Index()
-	// Create a new variable identifier with the name "v"
-	indexedSource := xtype.VariableID(jen.Id("v"))
-
-	// Wrap the error message with the current index
-	errWrapper := Wrap("error setting index %d", jen.Id(index))
-	// Build the conversion source code for the given source and target type
-	_, id, err := gen.Build(ctx, indexedSource, source.ListInner, target.ListInner, errWrapper)
-	// If there is an error, lift it with the path information and return
-	if err != nil {
-		return nil, nil, err.Lift(&Path{
-			SourceID:   "[]",
-			SourceType: source.ListInner.String,
-			TargetID:   "[]",
-			TargetType: target.ListInner.String,
-		})
-	}
-
 	// Set the error target variable to nil in the context
 	ctx.SetErrorTargetVar(jen.Nil())
 
 	// Split the raw field settings from the context by space
+	// This is done to extract individual settings from a single string.
 	split := strings.Split(ctx.Conf.RawFieldSettings[0], " ")
-	// Get the second and third elements from the split result
+
+	// Check if the split result has less than 3 elements.
+	// If it does, return nil for all return values.
+	// This is because we expect at least 3 elements from the split operation.
+	// The first element is ignored, and the second and third elements are used.
 	if len(split) < 3 {
-		return nil, nil, err.Lift(&Path{
-			SourceID:   "[]",
-			SourceType: source.ListInner.String,
-			TargetID:   "[]",
-			TargetType: target.ListInner.String,
-		})
+		return nil, nil, &Error{
+			Cause: "Invalid settings for goverter:agg",
+		}
 	}
+
+	// Extract the second element from the split result and assign it to the variable "mark".
+	// This represents the first relevant setting from the raw field settings.
 	mark := split[1]
+
+	// Extract the third element from the split result and assign it to the variable "mark2".
+	// This represents the second relevant setting from the raw field settings.
 	mark2 := split[2]
 
 	// Initialize an xtype.Type for the source
@@ -81,53 +70,74 @@ func (*Agg) Build(gen Generator, ctx *MethodContext, sourceID *xtype.JenID, sour
 		}
 	}
 
-	// Define the result block of code
-	resulBlock := []jen.Code{
-		// Try to get the value from the map with the key "v.sFType"
-		jen.List(jen.Id("found"), jen.Id("ok")).Op(":=").Id("i").Index(jen.Id("v").Dot(sFType.String)),
-		// If the key is not found, continue to the next iteration
+	// blockIter is a slice of jen.Code. It represents a block of code that is generated dynamically.
+	// This block of code is used to create a new instance of the target struct type.
+	// The fields of the new instance are populated based on the fields of the source struct type.
+	blockIter := []jen.Code{
+		// jen.Id(target.ListInner.String).ValuesFunc creates a new instance of the target struct type.
+		// The function passed to ValuesFunc is used to populate the fields of the new instance.
+		jen.Id(target.ListInner.String).ValuesFunc(func(g *jen.Group) {
+			// This loop iterates over the fields of the source struct type.
+			for i := 0; i < source.ListInner.StructType.NumFields(); i++ {
+				// If the name of the current field matches mark2, a new slice of strings is created and assigned to the field.
+				if source.ListInner.StructType.Field(i).Name() == mark2 {
+					g.Id(source.ListInner.StructType.Field(i).Name()).Op(":").Index().String().Values()
+					continue
+				}
+				// If the name of the current field does not match mark2, the value of the field in the source struct is assigned to the corresponding field in the target struct.
+				g.Id(target.ListInner.StructType.Field(i).Name()).Op(":").Id("v").Dot(source.ListInner.StructType.Field(i).Name())
+			}
+		}),
+	}
+	// resultBlock is a slice of jen.Code that represents a block of code for handling the result of the conversion.
+	resultBlock := []jen.Code{
+		// This line tries to get the value from the map "m" with the key "v.sFType".
+		// "found" is the value found in the map, and "ok" is a boolean indicating whether the key was found in the map.
+		jen.List(jen.Id("found"), jen.Id("ok")).Op(":=").Id("m").Index(jen.Id("v").Dot(sFType.String)),
+		// This conditional statement checks if the key was not found in the map. If the key was not found, it continues to the next iteration of the loop.
 		jen.If(jen.Op("!").Id("ok")).Block(
 			jen.Continue(),
 		),
-		// Delete the key "v.sFType" from the map
-		jen.Delete(jen.Id("i"), jen.Id("v").Dot(sFType.String)),
-		// Append the found value to the result
+		// This line deletes the key "v.sFType" from the map "m".
+		jen.Delete(jen.Id("m"), jen.Id("v").Dot(sFType.String)),
+		// This line appends the value found in the map to the "result" variable.
 		jen.Id("result").Op("=").Append(jen.Id("result"), jen.Id("found")),
 	}
 	// Define the block of code
 	block := []jen.Code{
-		// Try to get the value from the map with the key "v.sFType"
+		// This line attempts to retrieve a value from the map "m" using the key "v.sFType".
+		// The underscore "_" is a blank identifier, used when syntax requires a variable name but program logic does not.
+		// "ok" is a boolean that is true if the key was found in the map, and false otherwise.
 		jen.If(
-			jen.List(jen.Id("_"), jen.Id("ok")).Op(":=").Id("i").Index(jen.Id("v").Dot(sFType.String)),
+			jen.List(jen.Id("_"), jen.Id("ok")).Op(":=").Id("m").Index(jen.Id("v").Dot(sFType.String)),
 			jen.Id("!ok"),
 		).Block(
-			// If the key is not found, set the value to the generated code
-			jen.Id("i").Index(jen.Id("v").Dot(sFType.String)).Op("=").Add(id.Code),
-			// Get the object from the map with the key "v.sFType.String"
-			jen.Id("obj").Op(":=").Id("i").Index(jen.Id("v").Dot(sFType.String)),
-			// Set the "obj.tFType.String" to an empty string slice
-			jen.Id("obj").Dot(tFType.String).Op("=").Index().String().Values(),
-			// Set the value in the map with the key "v.sFType" to the object
-			jen.Id("i").Index(jen.Id("v").Dot(sFType.String)).Op("=").Id("obj"),
+			// If the key was not found in the map ("ok" is false), this line sets the value in the map with the key "v.sFType" to the target type.
+			// The target type is represented by the blockIter slice of jen.Code.
+			jen.Id("m").Index(jen.Id("v").Dot(sFType.String)).Op("=").Add(blockIter...),
 		),
-		// Get the object from the map with the key "v.sFType"
-		jen.Id("obj").Op(":=").Id("i").Index(jen.Id("v").Dot(sFType.String)),
-		// Append "v.tFType.String" to "obj.tFType.String"
+		// This line retrieves the object from the map "m" using the key "v.sFType" and assigns it to the variable "obj".
+		jen.Id("obj").Op(":=").Id("m").Index(jen.Id("v").Dot(sFType.String)),
+		// This line appends the string "v.tFType.String" to the slice "obj.tFType.String".
 		jen.Id("obj").Dot(tFType.String).Op("=").Append(jen.Id("obj").Dot(tFType.String), jen.Id("v").Dot(tFType.String)),
-		// Set the value in the map with the key "v.sFType" to the object
-		jen.Id("i").Index(jen.Id("v").Dot(sFType.String)).Op("=").Id("obj"),
+		// This line sets the value in the map with the key "v.sFType" to the object "obj".
+		jen.Id("m").Index(jen.Id("v").Dot(sFType.String)).Op("=").Id("obj"),
 	}
 
 	// Define the SMTP block of code
 	smtp := []jen.Code{
-		// Declare a variable "result" with the type of the target
+		// This line declares a variable "result" with the type of the target.
 		jen.Var().Id("result").Id(target.String),
-		// Declare a map "i" with the key type of xType and value type of str
-		jen.Id("i").Op(":=").Map(sFType.TypeAsJen()).Add(jen.Id(target.ListInner.String)).Values(),
-		// Iterate over the source
+		// This line declares a map "m" with the key type of sFType (source field type) and value type of target.ListInner.String.
+		jen.Id("m").Op(":=").Map(sFType.TypeAsJen()).Add(jen.Id(target.ListInner.String)).Values(),
+		// This loop iterates over the source. For each iteration, it executes the block of code defined earlier.
+		// The underscore "_" is a blank identifier, used when syntax requires a variable name but program logic does not.
+		// "v" is the value of the current iteration.
 		jen.For(jen.List(jen.Id("_"), jen.Id("v")).Op(":=").Range().Id("source")).Block(block...),
-		// Iterate over the source again
-		jen.For(jen.List(jen.Id("_"), jen.Id("v")).Op(":=").Range().Id("source")).Block(resulBlock...),
+		// This loop iterates over the source again. For each iteration, it executes the resultBlock of code defined earlier.
+		// The underscore "_" is a blank identifier, used when syntax requires a variable name but program logic does not.
+		// "v" is the value of the current iteration.
+		jen.For(jen.List(jen.Id("_"), jen.Id("v")).Op(":=").Range().Id("source")).Block(resultBlock...),
 	}
 
 	// Return the SMTP block of code, the result variable identifier, and nil for error
--- cmd/goverter/main.go
+++ cmd/goverter/main.go
@@ -5,24 +5,14 @@ import (
 	"os"
 
 	"github.com/emp1re/goverter-test"
-	"github.com/emp1re/goverter-test/config"
+	"github.com/emp1re/goverter-test/cli"
 )
 
 func main() {
-	//cfg, err := cli.Parse(os.Args)
-	//if err != nil {
-	//	_, _ = fmt.Fprintln(os.Stderr, err)
-	//	os.Exit(1)
-	//}
-	cfg := &goverter.GenerateConfig{
-		PackagePatterns: []string{""},
-		WorkingDir:      "/home/vsl/go/src/govert/",
-		Global: config.RawLines{
-			Lines:    nil,
-			Location: "global",
-		},
-		BuildTags:             "goverter",
-		OutputBuildConstraint: "!goverter",
+	cfg, err := cli.Parse(os.Args)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	if err := goverter.GenerateConverters(cfg); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
